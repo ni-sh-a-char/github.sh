3812646d77b9906838e86bea8b26642382187703
PIYUSH-MISHRA-00 <piyushmishra555@outlook.com>
2022-08-11 00:22:30 +0530

    git auto commit

--- github.sh
+++ github.sh
@@ -8,7 +8,6 @@ map["GIT-AUTOMATION"] = "Git-Automation"  # replace remote with remote you creat
 #map["remote"] = "original" # you can add as many remote and destination repository you want to push just like this
 
 git config --global user.name "PIYUSH-MISHRA-00" # replace username inside inverted comma with your GitHub user name
-git init
 git add .
 git commit -m "git auto commit"
 
