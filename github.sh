7861fecebbd27aa4764f6fadb48b79323d06b220
PIYUSH-MISHRA-00 <piyushmishra555@outlook.com>
2022-08-11 00:24:00 +0530

    git auto commit

--- github.sh
+++ github.sh
@@ -2,12 +2,13 @@
 
 declare -A map
 
-map["GIT-AUTOMATION"] = "Git-Automation"  # replace remote with remote you created and replace original with your repository you want to push the files
+map["Git-Automation"] = "Git-Automation"  # replace remote with remote you created and replace original with your repository you want to push the files
 #map["remote"] = "original" # you can add as many remote and destination repository you want to push just like this
 #map["remote"] = "original" # you can add as many remote and destination repository you want to push just like this
 #map["remote"] = "original" # you can add as many remote and destination repository you want to push just like this
 
 git config --global user.name "PIYUSH-MISHRA-00" # replace username inside inverted comma with your GitHub user name
+git init
 git add .
 git commit -m "git auto commit"
 
@@ -15,5 +16,4 @@ for i in "${!map[@]}"
 do
     git remote add $i https://github.com/PIYUSH-MISHRA-00/${map[$i]}.git # replace "username" with your Github username
     git push -u $i main # you can replace main with the destination branch you want to select
-    git push
 done
