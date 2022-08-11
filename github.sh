d339d08a665e84ca0673527a13828fdb577844eb
PIYUSH-MISHRA-00 <piyushmishra555@outlook.com>
2022-08-12 01:06:15 +0530

    Added functionality to change the commit message by user input

--- github.sh
+++ github.sh
@@ -2,7 +2,7 @@
 
 declare -A map
 
-map["Git-Automation"] = "Git-Automation-1"  # replace remote with remote you created and replace original with your repository you want to push the files
+map["Git-Automation"] = "Git-Automation"  # replace remote with remote you created and replace original with your repository you want to push the files
 #map["remote"] = "original" # you can add as many remote and destination repository you want to push just like this by changing the remote and original inside the inverted comma.
 #map["remote"] = "original" # you can add as many remote and destination repository you want to push just like this
 #map["remote"] = "original" # you can add as many remote and destination repository you want to push just like this
@@ -17,7 +17,7 @@ git commit -m $echo "$message"
 
 for i in "${!map[@]}"
 do
-    git remote add $i https://github.com/The-Lord-Buddha-Club/${map[$i]}.git # replace "The-Lord-Buddha-Club" with your Github username
+    git remote add $i https://github.com/PIYUSH-MISHRA-00/${map[$i]}.git # replace "PIYUSH-MISHRA-00" with your Github username
     git push -u $i main # you can replace main with the destination branch you want to select
 done
 
